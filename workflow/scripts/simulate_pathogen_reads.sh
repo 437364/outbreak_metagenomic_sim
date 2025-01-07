40666fe9b9845e2bb88780ebc88709aef458c764
Marie Kratka <437364@gmail.com>
2025-01-07 18:17:12 +0100

    in silico seq commands

---
+++ workflow/scripts/simulate_pathogen_reads.sh
@@ -0,0 +1,4 @@
+module add mambaforge-22.9.0
+mamba activate /storage/brno2/home/kratka/.conda/envs/snakemake
+cd /storage/brno2/home/kratka/outbreak_metagenomic_sim/
+iss generate -p 4 --seed 1 -g results/pathogens/nipah/GCF_000863625.1_ViralProj15443_genomic.fna -n 100000 -m NovaSeq -o results/pathogens/nipah/nipah_novaseq_100k
